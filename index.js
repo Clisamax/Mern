6f2f5e147051b9951d18d3d5df3a81142e4e5420
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-11-20 10:36:38 -0300

    correção no index

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const userRoute = require("./src/routes/user.route");
 
 const app = express();
 
-app.use("/user", userRoute);
-
 const port = 3000;
-
-app.use(express.json());
+app.use(express.json())
+app.use("/user", userRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
