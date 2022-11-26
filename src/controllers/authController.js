3bff27075f0a77600884ca786c77dd4c247a54d4
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-11-26 04:40:12 -0300

    login ok

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,24 @@
-//import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service";
+import bcrypt from "bcrypt";
+import { loginService } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await  loginService(email);
+    const user = await loginService(email);
 
-    //const passwordIsValid = bcrypt.compare(password, user.password);
+    if(!user){
+      return res.status(404).send({message: "Password or User not found"})
+    }
 
-    res.send( user );
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    console.log(passwordIsValid);
+
+    if(!passwordIsValid){
+      return res.status(404).send({message: "Password or User not found"})
+    }
+
+    res.send(user);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 const router = Router();
+
 import userController from "../controllers/user.controller.js ";
 import { validId, validUser } from "../middlewares/global.middlewares.js";
 
