d5d34e976dc4a684a26b2f02545e295094f8407f
Clisamax <99000834+Clisamax@users.noreply.github.com>
2022-11-26 17:06:24 -0300

    jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -7,18 +7,20 @@ const login = async (req, res) => {
   try {
     const user = await loginService(email);
 
-    if(!user){
-      return res.status(404).send({message: "Password or User not found"})
+    if (!user) {
+      return res.status(404).send({ message: "Password or User not found" });
     }
 
     const passwordIsValid = bcrypt.compareSync(password, user.password);
     console.log(passwordIsValid);
 
-    if(!passwordIsValid){
-      return res.status(404).send({message: "Password or User not found"})
+    if (!passwordIsValid) {
+      return res.status(404).send({ message: "Password or User not found" });
     }
 
-    res.send(user);
+    const token = generateToken(user.id);
+
+    res.send({ token });
   } catch (err) {
     res.status(500).send(err.message);
   }
